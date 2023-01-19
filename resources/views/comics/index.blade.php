3544a956fc530084480dd5bbf7e3c5d723dba09a
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2023-01-19 15:27:45 +0100

    first commit

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.main')
+
+@section('page-content')
+    <div class="container">
+        <h1>Lista Comics</h1>
+        <div class="row">
+            @foreach ($comics as $comic)
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">{{$comic->title}}</h5>
+                          <p class="card-text">Price: {{$comic->price}}</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                      </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
